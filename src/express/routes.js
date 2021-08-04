944024cf075c0a3d37e0ecc1152746b5a38280aa
Ian Guedes <82589606+ianvgs@users.noreply.github.com>
2021-08-03 21:56:34 -0300

    Update routes.js

--- src/express/routes.js
+++ src/express/routes.js
@@ -9,15 +9,8 @@ module.exports = app => {
     Crud.consultar(req,res)
   }) 
   app.get('/', (req, res)=>{    
-    res.send("Você acessou o projeto facile-backend, consulte o manual no README para verificar os métodos disponíveis.").status(200)
-  })
-
-  app.get('/html', (req, res)=>{    
-    res.sendFile('index.html',{root: __dirname})
-  })
-  app.get('/geta', (req, res)=>{    
-    res.send("vamos ver").status(200)
-  })
+    res.send("Você acessou o backend do projeto Guedestocks, consulte o manual no README para verificar os métodos disponíveis.").status(200)
+  }) 
 
 }
 
